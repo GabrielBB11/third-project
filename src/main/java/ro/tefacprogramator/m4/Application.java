d9302c782a409fd734c46139100c542ef317742f
Burada Gabriel <gabrielburada6@gmail.com>
2024-04-20 10:48:28 +0300

    Added substract function

--- src/main/java/ro/tefacprogramator/m4/Application.java
+++ src/main/java/ro/tefacprogramator/m4/Application.java
@@ -5,8 +5,12 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("New program");
 
-        int rez = Calculator.add(100, 45);
+        int rezAdd = Calculator.add(100, 45);
         
-        System.out.println("rez = " + rez);
+        System.out.println("rez = " + rezAdd);
+
+        int rezSubstract = Calculator.add(100, 45);
+        
+        System.out.println("rez = " + rezSubstract);
     }
 }
